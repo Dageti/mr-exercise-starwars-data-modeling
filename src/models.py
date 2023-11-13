5e2383a42996d16dc4224567b44d8e8be027c8c4
Matias Rivas <mat500.600@gmail.com>
2023-11-13 15:53:21 -0300

    avance agregado planets

--- src/models.py
+++ src/models.py
@@ -25,6 +25,18 @@ class Address(Base):
     person_id = Column(Integer, ForeignKey('person.id'))
     person = relationship(Person)
 
+class Planet(Base):
+    __tablename__ = 'address'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    street_name = Column(String(250))
+    street_number = Column(String(250))
+    post_code = Column(String(250), nullable=False)
+    person_id = Column(Integer, ForeignKey('person.id'))
+    person = relationship(Person)
+
+
     def to_dict(self):
         return {}
 
